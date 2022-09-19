998f5a32198f23b78caf8f16d59afb71c78508c0
Maschell <Maschell@gmx.de>
2022-09-19 17:48:14 +0200

    Improve the error message on error

--- src/modpackSelector.h
+++ src/modpackSelector.h
@@ -5,4 +5,4 @@
 
 void HandleMultiModPacks(uint64_t titleid /*,bool showMenu = true*/);
 void console_print_pos(int x, int y, const char *format, ...);
-bool ReplaceContent(const std::string &basePath);
+bool ReplaceContent(const std::string &basePath, const std::string &modpack);
