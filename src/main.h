a8bc0bf0df42008c01e95732192f49a8e1270cbd
Maschell <Maschell@gmx.de>
2022-10-11 15:10:13 +0200

    Update version string

--- src/main.h
+++ src/main.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "version.h"
 
-#define VERSION                                "v0.1.1"
+#define VERSION                                "v0.1.2"
 #define VERSION_FULL                           VERSION VERSION_EXTRA
 
 #define AUTO_APPLY_SINGLE_MODPACK_STRING       "autoApplySingleModpack"
